5b6cff8bacf285f94a57094bc3a3d1390892de84
huongnv75 <huongnv75@outlook.com>
2021-09-22 22:57:48 +0700

    add deployment

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd ../web/
-docker build --tag shinobi-image:1.0 .
 docker stop open-camera
 docker rm open-camera
+docker rmi shinobi-image:1.0
+docker build --tag shinobi-image:1.0 .
 docker run -d --name='open-camera' -p '8080:8080/tcp' -v "/dev/shm/open-camera/web/streams":'/dev/shm/streams':'rw' -v "$HOME/open-camera/web/config":'/config':'rw' -v "$HOME/open-camera/web/customAutoLoad":'/home/open-camera/web/libs/customAutoLoad':'rw' -v "$HOME/open-camera/web/database":'/var/lib/mysql':'rw' -v "$HOME/open-camera/web/videos":'/home/open-camera/web/videos':'rw' -v "$HOME/open-camera/web/plugins":'/home/open-camera/web/plugins':'rw' -v '/etc/localtime':'/etc/localtime':'ro' shinobi-image:1.0
-
+docker logs -f open-camera
